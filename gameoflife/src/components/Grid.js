297b2fbe4447cf8e4b9b551e435bc8d07fcb9941
Logan Negley <logannegley@gmail.com>
2020-07-22 21:13:24 -0500

    grid layout is set working on onClick function of cell

--- gameoflife/src/components/Grid.js
+++ gameoflife/src/components/Grid.js
@@ -1,9 +1,29 @@
-import React from 'react':
+import React, {useState} from 'react';
 
 function Grid(){
-    return (
-        <div className ="grid_container">
+    const [gridCells, setGridCells] = useState([])
+    const [isAlive, setIsAlive] = useState(false)
 
+    const grid = []
+    for(let i = 1; i <= 625; i++){
+        grid.push(Array.from(Array(625), () => 0))
+    }; 
+
+    function changeCellState()=>{
+        
+    };
+
+    return (
+        <div className = "board">
+            <div className = "cell_container">
+                {grid.map((item, i) => (
+                    <div className="cell" 
+                    key={i}
+                    onClick = {changeCellState}>
+                        {/* empty divs as cells with value of 0 */}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
