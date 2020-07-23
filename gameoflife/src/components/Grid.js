1fb77310dad0cdbc61a1634dabdb502ba349770b
Logan Negley <logannegley@gmail.com>
2020-07-23 16:55:24 -0500

    added comments to code

--- gameoflife/src/components/Grid.js
+++ gameoflife/src/components/Grid.js
@@ -1,26 +1,29 @@
 import React, {useState} from 'react';
 
 function Grid(){
-    const [gridCells, setGridCells] = useState([])
     const [isAlive, setIsAlive] = useState(false)
 
+// Grid array to hold cells
     const grid = []
+    // Creating grid with 25x25 cells
     for(let i = 1; i <= 625; i++){
-        grid.push(Array.from(Array(625), () => 0))
+        grid.push(Array.from(Array(625), () => isAlive))
     }; 
 
-    function changeCellState()=>{
-        
+// onclick to change weather cell is alive or not
+    const changeCellState= event => {
+        event.preventDefault();
+        setIsAlive(!isAlive)
     };
 
     return (
         <div className = "board">
             <div className = "cell_container">
                 {grid.map((item, i) => (
-                    <div className="cell" 
+                    <div 
+                    className={!isAlive ? 'cell' : 'live' } 
                     key={i}
                     onClick = {changeCellState}>
-                        {/* empty divs as cells with value of 0 */}
                     </div>
                 ))}
             </div>
