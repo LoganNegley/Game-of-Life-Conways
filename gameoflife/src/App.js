2b247459b7400056f78e2b4a6fe283973ec396f9
Logan Negley <logannegley@gmail.com>
2020-07-24 14:09:21 -0500

    changed button context to controls context

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -4,6 +4,8 @@ import Grid from '../src/components/game/Grid';
 import GenerationCount from '../src/components/controls/Generation';
 import Buttons from '../src/components/controls/Buttons';
 
+import ButtonContext from '../src/context/ControlsContext';
+
 function App() {
 const [generation, setGeneration] = useState(0)
 const [random, setRandom] = useState(false)
@@ -11,6 +13,7 @@ const [random, setRandom] = useState(false)
   return (
     <div className="App">
       <h1>Game Of Life</h1>
+
       <Buttons random={random}/>
       <GenerationCount generation={generation}/>
       <Grid/>
