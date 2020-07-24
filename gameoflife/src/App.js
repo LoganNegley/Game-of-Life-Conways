6d6dea979b6e5cb8bb00992264e1da8b08d512f1
Logan Negley <logannegley@gmail.com>
2020-07-24 14:38:20 -0500

    setting up state managment with controls

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import Grid from '../src/components/game/Grid';
-import GenerationCount from '../src/components/controls/Generation';
+
 import Buttons from '../src/components/controls/Buttons';
 
 // imported context files for app
@@ -10,13 +10,14 @@ import ControlsContext from '../src/context/ControlsContext';
 function App() {
 const [generation, setGeneration] = useState(0)
 const [random, setRandom] = useState(false)
+const [startSimulation, setStartSimulation]= useState(false)
 
   return (
     <div className="App">
       <h1>Game Of Life</h1>
-      <ControlsContext.Provider value= {random,generation}>
-        <Buttons random={random}/>
-        <GenerationCount generation={generation}/>
+      <ControlsContext.Provider value= {{random,generation, startSimulation}}>
+        <Buttons/>
+        
         <Grid/>
       </ControlsContext.Provider>
     </div>
--- gameoflife/src/components/controls/Buttons.js
+++ gameoflife/src/components/controls/Buttons.js
@@ -1,14 +1,20 @@
 import React, {useState, useContext} from 'react';
+// Component imports
+import GenerationCount from '../controls/Generation';
+// Context imports
 import ControlsContext from './../../context/ControlsContext';
 
-function ControlButtons(props) {
+function ControlButtons() {
+    // State managment from ControlsContext
     const {random, generation} = useContext(ControlsContext)
+
   return (
     <div className="control_buttons">
         <button>Start</button>
         <button>Stop</button>
         <button>Clear</button>
         <button>Random</button>
+        <GenerationCount generation={generation}/>
     </div>
   );
 }
