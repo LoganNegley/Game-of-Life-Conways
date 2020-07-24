9ec42e53c7e6b1f3b9e38aefa5a471521b02d946
Logan Negley <logannegley@gmail.com>
2020-07-23 19:21:31 -0500

    grid with all cells showing up

--- gameoflife/src/components/Grid.js
+++ gameoflife/src/components/Grid.js
@@ -7,16 +7,24 @@ function Grid(){
     const [isAlive, setIsAlive] = useState(false)
     const [grid, setGrid] = useState(Array(rowNum).fill().map(() => Array(colsNum).fill(false)))
 
-    const rows = []
-    for (var r = 0; r < rowNum; r++){
-        for (var c = 0; c < colsNum; c++){
-            rows.push(grid)
-        }
-    }
+    console.log(grid)
+
+    // const rows = []
+    // for (var r = 0; r < rowNum; r++){
+    //     for (var c = 0; c < colsNum; c++){
+    //         rows.push(grid)
+    //     }
+    // }
 
     return (
         <div className = "board">
             <div className = "cell_container">
+                {grid.map((rows,r) =>
+                    rows.map((col,c) => 
+                    <div className="cell">
+                        
+                    </div>
+                ))}
             </div>
         </div>
     );
