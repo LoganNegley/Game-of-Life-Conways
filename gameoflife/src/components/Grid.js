e54781a8e7b8a85b6fe09fa7b1979b749a94e4a8
Logan Negley <logannegley@gmail.com>
2020-07-23 19:30:13 -0500

    grid showing with a hover effect on each cell

--- gameoflife/src/components/Grid.js
+++ gameoflife/src/components/Grid.js
@@ -5,24 +5,17 @@ let colsNum= 25
 
 function Grid(){
     const [isAlive, setIsAlive] = useState(false)
-    const [grid, setGrid] = useState(Array(rowNum).fill().map(() => Array(colsNum).fill(false)))
-
-    console.log(grid)
-
-    // const rows = []
-    // for (var r = 0; r < rowNum; r++){
-    //     for (var c = 0; c < colsNum; c++){
-    //         rows.push(grid)
-    //     }
-    // }
+    const [grid, setGrid] = useState(Array(rowNum).fill().map(() => Array(colsNum).fill(isAlive)))
 
     return (
         <div className = "board">
             <div className = "cell_container">
                 {grid.map((rows,r) =>
                     rows.map((col,c) => 
-                    <div className="cell">
-                        
+                    <div className="cell" 
+                    key={`${r}-${c}`}
+                    >
+                        {grid[r]}
                     </div>
                 ))}
             </div>
