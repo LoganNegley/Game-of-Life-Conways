e8fcf2c368e93999ba6b280bb4fa6707e2e72a67
Logan Negley <logannegley@gmail.com>
2020-07-30 09:18:07 -0500

    cleaning up code to make cleaner

--- gameoflife/src/components/game/Grid.js
+++ gameoflife/src/components/game/Grid.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {CloneGrid} from '../../helpers/helperFunc';
-import findNeighbors from '../data/data';
+import {CloneGrid, generateRandom, simulation} from '../../helpers/helperFunc';
 import GridSize from '../game/GridSize';
 
 function Grid(props){
@@ -9,87 +8,7 @@ function Grid(props){
     const [grid, setGrid] = useState(Array(rowNum).fill().map(() => Array(colsNum).fill(false)))
     const [interval, setInterval] = useState('')
 
-// Runs simulation of game
-    const runGame = ()=>{
-        let newGrid = grid
-        let copyGrid = CloneGrid(grid)
-        for(let r = 0; r < rowNum; r++){ //going through each cell in grid
-            for(let c = 0; c < colsNum; c++){
-                let neighbors = 0;
-                if(r > 0){
-                     if(newGrid[r - 1][c]){
-                         neighbors++;
-                     } 
-                }
-                if(r > 0 && c >0){
-                    if(newGrid[r-1][c-1]){
-                        neighbors++
-                    }
-                }
-                if(r > 0 && c < colsNum - 1){
-                    if(newGrid[r-1][c +1]){
-                        neighbors ++
-                    }
-                }
-                if(r < rowNum -1){
-                    if(newGrid[r + 1][c]){
-                        neighbors++
-                    }
-                }
-                if(r < rowNum - 1 && c > 0){
-                    if(newGrid[r + 1][c - 1]){
-                        neighbors++
-                    }
-                }
-                if(r < rowNum - 1 && colsNum - 1){
-                    if(newGrid[r + 1][c + 1]){
-                        neighbors++
-                    }
-                }
-                if(c < colsNum - 1){
-                    if(newGrid[r][c +1]){
-                        neighbors++
-                    }
-                }
-                if(c > 0){
-                    if(newGrid[r][c - 1]){
-                        neighbors++
-                    }
-                }
-
-                if(newGrid[r][c] && (neighbors < 2 || neighbors > 3)) copyGrid[r][c] = false;
-                if(!newGrid[r][c] && neighbors === 3) copyGrid[r][c] =true //if cell is dead and has 3 neighbors it is born
-            }
-        }
-
-
-        setGrid(copyGrid)
-        props.setGeneration(props.generation + 1)
-        console.log('running')
-        setInterval(setTimeout(runGame, 1000))
-    };
-
-
-// Stops simulation of game
-    const stopGame =()=>{
-        setInterval(clearTimeout(interval))
-        console.log('game stopped')
-    };
-    
-
-// Populate board with random live cells
-    const randomBoard = ()=>{
-        let newGrid = CloneGrid(grid)
-        for(let a = 0; a < rowNum; a++){
-            for(let b = 0; b < colsNum; b++){
-                   if(Math.random() < 0.5/2){
-                    newGrid[a][b] = true;
-                }
-            }}
-        setGrid(newGrid)
-    };
-
-// Runs when running state changes
+    // Runs when running state changes
     useEffect(()=>{
         if(props.running){
             runGame();
@@ -118,6 +37,25 @@ function Grid(props){
         props.setGeneration(0)
         setGrid(newGrid)
     },[props.isClear])
+
+// Runs simulation of game
+    const runGame = ()=>{
+        setGrid(simulation(grid, rowNum, colsNum)) //helper function run simulation
+        props.setGeneration(props.generation + 1)
+        console.log('running')
+        setInterval(setTimeout(runGame, 1000))
+    };
+
+// Stops simulation of game
+    const stopGame =()=>{
+        setInterval(clearTimeout(interval))
+        console.log('game stopped')
+    };
+    
+// Random board function
+    const randomBoard = () =>{
+        setGrid(generateRandom(grid, rowNum, colsNum)) //helper func to make board random
+    };
 
 
     return (
