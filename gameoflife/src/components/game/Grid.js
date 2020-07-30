8290518416f56e81edd95762906b92a3239d4ff0
Logan Negley <logannegley@gmail.com>
2020-07-29 20:16:06 -0500

    got simulation to work on hiting start but isn't going

--- gameoflife/src/components/game/Grid.js
+++ gameoflife/src/components/game/Grid.js
@@ -8,22 +8,66 @@ function Grid(props){
     const [colsNum, setColsNum] = useState(25)
     const [grid, setGrid] = useState(Array(rowNum).fill().map(() => Array(colsNum).fill(false)))
     const [interval, setInterval] = useState('')
+
 // Runs simulation of game
     const runGame = ()=>{
-        grid.map((row, r)=>{     //for each row in grid
-            row.map((col, c) =>{   // for each col in row
-                const newGrid = CloneGrid(grid)  //Create a new grid
+        let newGrid = grid
+        let copyGrid = CloneGrid(grid)
+        for(let r = 0; r < rowNum; r++){
+            for(let c = 0; c < colsNum; c++){
                 let neighbors = 0;
-                console.log(newGrid)
+                if(r > 0){
+                     if(newGrid[r - 1][c]){
+                         neighbors++;
+                     } 
+                }
+                if(r > 0 && c >0){
+                    if(newGrid[r-1][c-1]){
+                        neighbors++
+                    }
+                }
+                if(r > 0 && c < colsNum - 1){
+                    if(newGrid[r-1][c +1]){
+                        neighbors ++
+                    }
+                }
+                if(r < rowNum -1){
+                    if(newGrid[r + 1][c]){
+                        neighbors++
+                    }
+                }
+                if(r < rowNum - 1 && c > 0){
+                    if(newGrid[r + 1][c - 1]){
+                        neighbors++
+                    }
+                }
+                if(r < rowNum - 1 && colsNum - 1){
+                    if(newGrid[r + 1][c + 1]){
+                        neighbors++
+                    }
+                }
+                if(c < colsNum - 1){
+                    if(newGrid[r][c +1]){
+                        neighbors++
+                    }
+                }
+                if(c > 0){
+                    if(newGrid[r][c - 1]){
+                        neighbors++
+                    }
+                }
+
+                if(newGrid[r][c] && (neighbors < 2 || neighbors > 3)) copyGrid[r][c] = false;
+                if(!newGrid[r][c] && neighbors === 3) copyGrid[r][c] =true
+            }
+        }
 
 
-
-
-            });
-        });
+        setGrid(copyGrid)
         console.log('running')
         setInterval(setTimeout(runGame, 1000))// Conditional to simulate game
     };
+
 
 // Stops simulation of game
     const stopGame =()=>{
